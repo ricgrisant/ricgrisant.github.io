2449d340efbeefb9fefdd798889c7ce9c280ccd8
Ricardo <ricardogrijalva10@gmail.com>
2021-05-20 01:01:42 -0600

    My portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// Go to top button
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Animation on Scroll
+
+AOS.init({
+    offset: 120, 
+    delay: 0, 
+    duration: 1000, 
+    easing: 'ease',
+    once: true,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+  
+  });
+
+  var typed = new Typed('.typed', {
+    strings: ["Web Developer.","Web Designer."],
+    typeSpeed: 70,
+    backDelay: 1500,
+    backSpeed: 30,
+    loop: true,
+    loopCount: 4
+  });
